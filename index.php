229312d2c6e6184b6291cc0fffa69a3130db1c69
NikhilNar <nik.nar123@gmail.com>
2016-11-03 01:07:41 +0530

    Added message if the weather was not found from weatherforecast.com

--- index.php
+++ index.php
@@ -31,9 +31,13 @@ include("find-weather.php");
 
     <div id="weather">
         <?php
-            if(isset($weather)){
+            if(isset($weather) && $weather!=''){
                 echo "<h4>". "Weather of ".$_GET['city']."</h4>";
                 echo '<div class="alert alert-success" role="alert">'.$weather.'</div>';
+            }
+            else if($weather=='' && isset($_GET['city']) ){
+                echo '<div class="alert alert-info" role="alert">Sorry weather not found for the selected city '.$_GET['city'].'</div>';
+
             }
             else if(isset($_GET['city'])){
                echo' <div class="alert alert-danger" role="alert">No such city</div>';
@@ -49,10 +53,7 @@ include("find-weather.php");
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
-<script type="text/javascript" >
-    window.onload=function(){
-        document.getElementById("main-form").style.display="block";
-    }
+<script type="text/javascript" src="javascript/weather-teller.js">
 </script>
 </body>
 </html>
